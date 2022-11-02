00c8b80de429ce696c808b63054542e656a43df5
xu0808 <haohaoqiankunxu@163.com>
2022-11-02 22:57:19 +0800

    二分排序整理

--- src/base/leetcode/sort/Sort.java
+++ src/base/leetcode/sort/Sort.java
@@ -1,5 +1,15 @@
 package sort;
+import java.util.Arrays;
 
+/**
+ * 排序算法：
+ * 冒泡排序
+ * 选择排序
+ * 直接插入排序
+ * 二分法排序
+ * 希尔排序
+ * 快速排序
+ */
 public class Sort {
     public static void main(String[] args) {
         int[] array = new int[]{8, 7, 6, 5, 1, 1, 2, 3, 4};
@@ -13,7 +23,73 @@ public class Sort {
 
     }
 
-    // 二分排序:从小到大 (时间复杂度o(nlogn))
+    /**
+     * 冒泡排序：两两比较，大者交换位置，则每一圈比较最大的数就会冒到最后，循环直至遍历完所有
+     * 时间复杂度O（n²） 空间复杂度: O(1)   稳定性：稳定
+     */
+    private void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序：每次找出剩余序列最小值，循环直至遍历完所有
+     * 时间复杂度O（n²） 空间复杂度: O(1)   稳定性：不稳定
+     */
+    public static void selectionSort(int[] nums) {
+
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] > nums[j]) {
+                    swap(nums, i, j);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * 插入排序：
+     * 时间复杂度O（n²） 空间复杂度: O(1)   稳定性：不稳定
+     */
+    public static void insertSort(int[] arr) {
+        int temp;
+        for (int i=1;i<arr.length;i++){
+            // 待排元素小于有序序列的最后一个元素时，向前插入
+            if (arr[i]<arr[i-1]){
+                temp = arr[i];
+                for (int j=i;j>=0;j--){
+                    if (j>0 && arr[j-1]>temp) {
+                        arr[j]=arr[j-1];
+                    }else {
+                        arr[j]=temp;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    /**
+     * 二分排序:从小到大 (时间复杂度O(nlogn))
+     */
     public static void sort4_0(int[] array) {
         // 时间复杂度
         int o = 0;
