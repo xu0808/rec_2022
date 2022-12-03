2b17ddad92337f6bc35c6ae0b722af95a49b9fef
xu0808 <haohaoqiankunxu@163.com>
2022-12-03 16:27:11 +0800

    源石

--- src/base/leetcode/lesson/Sum.java
+++ src/base/leetcode/lesson/Sum.java
@@ -10,12 +10,16 @@ public class Sum {
                 32613,
                 32597,
                 38035,
-                199831};
+                199831
+        };
         // 521638
         int sum = 0;
         for (int i : arr) {
             sum += i;
         }
         System.out.println("sum = " + sum);
+        // 源石31 %
+        double up = (72.0 - 55) / 55.0;
+        System.out.println("up = " + String.format("%.6f", up));
     }
 }
